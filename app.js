8543b9dc7e204f787306f9e49b853b0ded089351
teenabobeena <christinas.codingemail@gmail.com>
2025-05-16 14:17:22 -0400

    Add numbers and style

---
+++ app.js
@@ -0,0 +1,51 @@
+// let num1 = "";
+// let num2 = "";
+// let operator = "";
+let display = "";
+
+
+//let first = parseInt(prompt("first number"));
+//let second = parseInt(prompt("second number"));
+
+function add(first, second) {
+    let sum = first + second;
+    display += sum;
+    console.log(display);
+    console.log(sum);
+}
+
+function subtract(first, second) {
+    let minus = first - second;
+    display += minus;
+    console.log(display);
+    console.log(minus);
+}
+
+function multiply(first, second) {
+    let product = first * second;
+    display += product;
+    console.log(display);
+    console.log(product);
+}
+
+function divide(first, second) {
+    let divided = first / second;
+    display += divided;
+    console.log(display);
+    console.log(divided);
+}
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
+
+console.log(operate(first, "-", second));
